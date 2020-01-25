5b18bb8a1add0519475190c55b3cd556024cf573
kevinfaeh <kevin.faeh@sunrise.ch>
2020-01-25 17:14:56 +0100

    changed frequency

--- ultrasonic_sensor/src/UltrasonicNode.py
+++ ultrasonic_sensor/src/UltrasonicNode.py
@@ -12,12 +12,13 @@ GPIO.setmode(GPIO.BCM)
 
 ultrasonic_sensor = UltrasonicSensor(1, 1, 0)
 
-rospy.loginfo("[Ultrasonic] starting node")
 rospy.init_node("ultrasonic")
-rate = rospy.Rate(20)
+rospy.loginfo("[Ultrasonic] starting node")
+rate = rospy.Rate(10)
 
 data_publisher = rospy.Publisher("ultrasonic/sensordata", Range, queue_size=3)
 message = Range()
+message.field_of_view = (10.0/180.0) * 3.141459265
 message.min_range = 0.02
 message.max_range = 2
 message.header.frame_id = "/sonar_ranger"
@@ -26,7 +27,6 @@ try:
     while not rospy.is_shutdown():
         result = ultrasonic_sensor.measure()
         message.range = result
-        message.field_of_view = 0
         data_publisher.publish(message)
         rate.sleep()
     GPIO.cleanup()
